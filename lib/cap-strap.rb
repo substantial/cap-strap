6d716878271f8bae19e305938c9252a546a04ac2
Shaun Dern <shaun@substantial.com>
2012-04-18 11:13:00 -0700

    renaming chef_solo to chef-solo

--- lib/cap-strap.rb
+++ lib/cap-strap.rb
@@ -2,5 +2,5 @@ require "cap-strap/version"
 
 require "cap-strap/tasks/rvm"
 require "cap-strap/tasks/bootstrap"
-require "cap-strap/tasks/chef_solo"
+require "cap-strap/tasks/chef-solo"
 require "cap-strap/tasks/users"
