16dfdf2e1cacfe71c49d334eb17f8ccfab85a765
Adam Smith <re5etsmyth@gmail.com>
2013-03-27 16:03:23 -0700

    Add possibly needed dependencies for rvm / ruby building

--- lib/cap-strap/recipes/bootstrap.rb
+++ lib/cap-strap/recipes/bootstrap.rb
@@ -8,7 +8,8 @@ module Capistrano::CapStrap
       rvm_packages = %w( build-essential openssl libreadline6 libreadline6-dev
           curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev
           libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf
-          libc6-dev ncurses-dev automake libtool bison subversion )
+          libc6-dev ncurses-dev automake libtool bison subversion
+          libgdbm-dev pkg-config libffi-dev )
 
       configuration.load do
 
